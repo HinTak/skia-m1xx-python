17baee358cbf5183dc5140aa115cd65efb5b16a8
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-05-21 02:28:05 +0100

    This code seems to run standalone

--- tests/test_grcontext.py
+++ tests/test_grcontext.py
@@ -597,10 +597,8 @@ def test_GrVkBackendContext_init():
     assert isinstance(skia.GrVkBackendContext(), skia.GrVkBackendContext)
 
 def test_ShaderError():
-    import contextlib, glfw, skia
+    import glfw
     from OpenGL import GL
-
-    WIDTH, HEIGHT = 640, 480
 
     path = skia.Path()
     path.moveTo(184, 445)
@@ -613,46 +611,37 @@ def test_ShaderError():
     path.quadTo(338, 292, 320, 280) # <- comment out this line and shape will draw correctly with anti-aliasing
     path.close()
 
-    @contextlib.contextmanager
-    def glfw_window():
-        if not glfw.init():
-            raise RuntimeError('glfw.init() failed')
-        glfw.window_hint(glfw.STENCIL_BITS, 8)
-        window = glfw.create_window(WIDTH, HEIGHT, '', None, None)
-        glfw.make_context_current(window)
-        yield window
-        glfw.terminate()
+    if not glfw.init():
+        raise RuntimeError('glfw.init() failed')
+    glfw.window_hint(glfw.STENCIL_BITS, 8)
+    window = glfw.create_window(640, 480, '', None, None)
+    glfw.make_context_current(window)
 
-    @contextlib.contextmanager
-    def skia_surface(window):
-        context = skia.GrDirectContext.MakeGL()
-        (fb_width, fb_height) = glfw.get_framebuffer_size(window)
-        backend_render_target = skia.GrBackendRenderTarget(
-            fb_width,
-            fb_height,
-            0,  # sampleCnt
-            0,  # stencilBits
-            skia.GrGLFramebufferInfo(0, GL.GL_RGBA8))
-        surface = skia.Surface.MakeFromBackendRenderTarget(
-            context, backend_render_target, skia.kBottomLeft_GrSurfaceOrigin,
-            skia.kRGBA_8888_ColorType, skia.ColorSpace.MakeSRGB())
-        assert surface is not None
-        yield surface
-        context.abandonContext()
+    context = skia.GrDirectContext.MakeGL()
+    (fb_width, fb_height) = glfw.get_framebuffer_size(window)
+    backend_render_target = skia.GrBackendRenderTarget(
+        fb_width,
+        fb_height,
+        0,  # sampleCnt
+        0,  # stencilBits
+        skia.GrGLFramebufferInfo(0, GL.GL_RGBA8))
+    surface = skia.Surface.MakeFromBackendRenderTarget(
+        context, backend_render_target, skia.kBottomLeft_GrSurfaceOrigin,
+        skia.kRGBA_8888_ColorType, skia.ColorSpace.MakeSRGB())
+    assert surface is not None
 
-    with glfw_window() as window:
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+    GL.glClear(GL.GL_COLOR_BUFFER_BIT)
 
-    with skia_surface(window) as surface:
-        with surface as canvas:
-            canvas.drawCircle(100, 100, 40, skia.Paint(Color=skia.ColorGREEN, AntiAlias=True))
+    with surface as canvas:
+        canvas.drawCircle(100, 100, 40, skia.Paint(Color=skia.ColorGREEN, AntiAlias=True))
 
-            paint = skia.Paint(Color=skia.ColorBLUE)
-            paint.setStyle(skia.Paint.kStroke_Style)
-            paint.setStrokeWidth(2)
-            paint.setAntiAlias(True)
+        paint = skia.Paint(Color=skia.ColorBLUE)
+        paint.setStyle(skia.Paint.kStroke_Style)
+        paint.setStrokeWidth(2)
+        paint.setAntiAlias(True)
 
-            canvas.drawPath(path, paint)
+        canvas.drawPath(path, paint)
 
-        surface.flushAndSubmit()
-        glfw.swap_buffers(window)
+    surface.flushAndSubmit()
+    import glfw
+    glfw.swap_buffers(window)
