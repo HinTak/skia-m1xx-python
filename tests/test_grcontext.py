86627f81c4721af7770e5fb5c098b805c64dd388
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-05-27 08:48:48 +0100

    funny drawing, but seems to work

--- tests/test_grcontext.py
+++ tests/test_grcontext.py
@@ -666,3 +666,56 @@ def test_ShaderError():
     surface.flushAndSubmit()
     import glfw
     glfw.swap_buffers(window)
+
+def test_ShaderError_2():
+    from OpenGL.GLUT import glutInit, glutInitDisplayMode, GLUT_DOUBLE, GLUT_RGBA, glutInitWindowSize, glutCreateWindow, \
+        glutSwapBuffers, glutPostRedisplay, glutDisplayFunc, glutMainLoop, glutMainLoopEvent
+    from OpenGL.GL import glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_RGBA8
+
+    path = skia.Path()
+    path.moveTo(184, 445)
+    path.lineTo(249, 445)
+    path.quadTo(278, 445, 298, 438)
+    path.quadTo(318, 431, 331, 419)
+    path.quadTo(344, 406, 350, 390)
+    path.quadTo(356, 373, 356, 354)
+    path.quadTo(356, 331, 347, 312)
+    path.quadTo(338, 292, 320, 280) # <- comment out this line and shape will draw correctly with anti-aliasing
+    path.close()
+
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
+    glutInitWindowSize(600, 480)
+    glutCreateWindow(b"OpenGL Window")
+
+    context = skia.GrDirectContext.MakeGL()
+
+    backend_render_target = skia.GrBackendRenderTarget(
+        600,
+        480,
+        0,  # sampleCnt
+        0,  # stencilBits
+        skia.GrGLFramebufferInfo(0, GL_RGBA8))
+    surface = skia.Surface.MakeFromBackendRenderTarget(
+        context, backend_render_target, skia.kBottomLeft_GrSurfaceOrigin,
+        skia.kRGBA_8888_ColorType, skia.ColorSpace.MakeSRGB())
+    assert surface is not None
+
+    def display():
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        with surface as canvas:
+            canvas.drawCircle(100, 100, 40, skia.Paint(Color=skia.ColorGREEN, AntiAlias=True))
+
+            paint = skia.Paint(Color=skia.ColorBLUE)
+            paint.setStyle(skia.Paint.kStroke_Style)
+            paint.setStrokeWidth(2)
+        paint.setAntiAlias(True)
+
+        canvas.drawPath(path, paint)
+
+    surface.flushAndSubmit()
+    glutSwapBuffers()
+    glutPostRedisplay()
+    glutDisplayFunc(display)
+    #glutMainLoop()
+    glutMainLoopEvent() # paint once then exit
