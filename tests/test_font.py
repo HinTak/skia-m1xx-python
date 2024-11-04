3c8e77500750c1294f55bb6e9102aaa9994987bd
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-11-04 08:12:58 +0000

    mac windows cannot load noto

--- tests/test_font.py
+++ tests/test_font.py
@@ -691,6 +691,11 @@ def test_ttc3():
 def color_emoji_run():
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     typeface = skia.Typeface.MakeFromFile(os.path.join(root_dir, "NotoColorEmoji.ttf"))
+    if (typeface == None): # Mac - it uses "Zapf Dingbats" otherwise ; apple does not support CBDT/CBLC
+        typeface = skia.Typeface("Apple Color Emoji")
+    import sys
+    if sys.platform.startswith("win"):
+        typeface = skia.Typeface("Segoe UI Emoji") # COLRv0
     text = "✌✌🏻"
     font = skia.Font(typeface,109)
     blob = skia.TextBlob.MakeFromShapedText(text, font)
