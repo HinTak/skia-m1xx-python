00e6bed08eee1d370d03b24ac104a6c201c405aa
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-11-27 20:08:47 +0000

    The newer Linux has /usr/bin/ninja and does not have python

--- scripts/build_Linux.sh
+++ scripts/build_Linux.sh
@@ -9,7 +9,6 @@ if [[ $(uname -m) == "aarch64" ]]; then
     yum -y install epel-release && \
         yum repolist && \
         yum install -y ninja-build && \
-        ln -s ninja-build /usr/bin/ninja &&
         mv depot_tools/ninja depot_tools/ninja.bak
 fi
 
@@ -36,7 +35,7 @@ export LDFLAGS="-lrt"
 git clone https://gn.googlesource.com/gn && \
     cd gn && \
     git checkout fe330c0ae1ec29db30b6f830e50771a335e071fb && \
-    python build/gen.py && \
+    python3 build/gen.py && \
     ninja -C out gn && \
     cd ..
 
